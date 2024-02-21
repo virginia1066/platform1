c94ad638522b639f775ef3082b1295a93bf2459c
tsigel <tsigel@gmail.com>
2024-02-21 09:08:29 +0300

    init front

---
+++ client/src/pages/DeckList/index.tsx
@@ -0,0 +1,10 @@
+import { PageWrap } from '../../components/PageWrap';
+import './DeckList.css';
+
+export const DeckList = () => {
+  return (
+    <PageWrap title='Ваши колодлы'>
+      <div>Body</div>
+    </PageWrap>
+  );
+}
