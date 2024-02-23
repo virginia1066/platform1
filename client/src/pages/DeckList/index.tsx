efe22ff5238d36fa985e3aff400bea83ef7bd325
tsigel <tsddaniil@gmail.com>
2024-02-23 13:17:39 +0300

    Add deck list to page

--- client/src/pages/DeckList/index.tsx
+++ client/src/pages/DeckList/index.tsx
@@ -2,12 +2,20 @@ import { useTranslation } from 'react-i18next';
 import { Block } from '../../components/Block/inedex';
 import { ButtonBar } from '../../components/ButtonBar';
 import { Header } from '../../components/Header';
-import { Button, Flex } from '@chakra-ui/react';
-import { useGate } from 'effector-react';
-import { DeckListGate } from '../../models/vocabulary';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { useGate, useList } from 'effector-react';
+import { $deckList, DeckListGate } from '../../models/vocabulary';
 
 export const DeckList = () => {
     useGate(DeckListGate);
+    const deck_list = useList($deckList, ({ id, name, count_review, count_learning, count_new, count_relearning }) => (
+        <Box key={`pack-${id}`}>
+            <p>{name}</p>
+            <p>New: {count_new}</p>
+            <p>Learning: {count_learning + count_relearning}</p>
+            <p>Review: {count_review}</p>
+        </Box>
+    ));
 
     const { t } = useTranslation('translation', {
         keyPrefix: 'vocabulary.deckList'
@@ -22,6 +30,7 @@ export const DeckList = () => {
             <div>
                 <Block>
                     {t('content')}
+                    {deck_list}
                 </Block>
             </div>
             <ButtonBar>
