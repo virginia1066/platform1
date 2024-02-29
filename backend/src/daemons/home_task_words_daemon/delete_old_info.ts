7d201971b7b52575d4bb2099925305546d44654a
tsigel <tsddaniil@gmail.com>
2024-02-29 15:23:12 +0300

    Fix update home task

--- backend/src/daemons/home_task_words_daemon/delete_old_info.ts
+++ backend/src/daemons/home_task_words_daemon/delete_old_info.ts
@@ -1,6 +1,7 @@
 import { knex } from '../../constants';
 import { LessonUpdate } from '../../types/Wokobular';
 import { head } from 'ramda';
+import { log_query } from '../../utils/log_query';
 
 export const delete_old_info = (lesson_id: number): Promise<unknown> => {
     return knex('lesson_updates')
@@ -16,15 +17,21 @@ export const delete_old_info = (lesson_id: number): Promise<unknown> => {
 
             return Promise
                 .all([
-                    knex('packs')
-                        .delete()
-                        .where('insert_id', update.lesson_id),
-                    knex('pack_links')
-                        .delete()
-                        .where('insert_id', update.lesson_id),
-                    knex('words')
-                        .delete()
-                        .where('insert_id', update.lesson_id)
+                    log_query(
+                        knex('packs')
+                            .where('insert_id', update.insert_id)
+                            .del()
+                    ),
+                    log_query(
+                        knex('pack_links')
+                            .where('insert_id', update.insert_id)
+                            .del()
+                    ),
+                    log_query(
+                        knex('words')
+                            .where('insert_id', update.insert_id)
+                            .del()
+                    )
                 ]);
         });
 };
