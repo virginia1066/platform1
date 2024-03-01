a1dbd6d04cef038738c82c4b27d9cc952b6926d9
tsigel <tsddaniil@gmail.com>
2024-03-01 12:38:40 +0300

    Add theme params

--- client/src/theme/defaults.ts
+++ client/src/theme/defaults.ts
@@ -1,9 +1,9 @@
 export const themeParams = {
-  "bg_color": "#ffffff",
-  "button_color": "#5AC8FB",
-  "button_text_color": "#ffffff",
-  "hint_color": "#8E8E8E",
-  "link_color": "#3E7FD2",
-  "secondary_bg_color": "#F3F2F8",
-  "text_color": "#020202",
-}
+    'bg_color': Telegram.WebApp.themeParams.bg_color ?? '#ffffff',
+    'button_color': Telegram.WebApp.themeParams.button_color ?? '#5AC8FB',
+    'button_text_color': Telegram.WebApp.themeParams.button_text_color ?? '#ffffff',
+    'hint_color': Telegram.WebApp.themeParams.hint_color ?? '#8E8E8E',
+    'link_color': Telegram.WebApp.themeParams.link_color ?? '#3E7FD2',
+    'secondary_bg_color': (Telegram.WebApp.themeParams as any).secondary_bg_color ?? '#F3F2F8',
+    'text_color': Telegram.WebApp.themeParams.text_color ?? '#020202',
+};
