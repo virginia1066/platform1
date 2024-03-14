b82604371b0c178bd55e8d83c42c3a9c760c313c
tsigel <tsddaniil@gmail.com>
2024-03-14 10:43:24 +0300

    Fix analytics

--- client/src/utils/send_analytics.ts
+++ client/src/utils/send_analytics.ts
@@ -1,15 +1,25 @@
 import { AMPLITUDE_API_KEY } from '../constants';
 import { init, track } from '@amplitude/analytics-browser';
+import { range } from 'ramda';
 
 type Props = {
     event_type: string;
     props?: Record<string, string | number | undefined>
 }
 
+const generate_id = () => {
+    if (window.crypto) {
+        return window.crypto.randomUUID();
+    }
+    return range(0, 5)
+        .map(() => Math.random())
+        .join('-');
+};
+
 const get_id = () => {
     const get_local_id = () => {
         try {
-            const id = localStorage.getItem('user_id') ?? crypto.randomUUID();
+            const id = localStorage.getItem('user_id') ?? generate_id();
             localStorage.setItem('user_id', id);
             return id;
         } catch (e) {
