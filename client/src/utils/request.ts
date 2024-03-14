01b49396519e3d3efa5f18a0befbfe4d02c2a781
tsigel <tsddaniil@gmail.com>
2024-03-14 17:56:34 +0300

    Fix session, move to headers

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -2,15 +2,17 @@ import { __, assoc, pipe } from 'ramda';
 import { BASE_URL } from '../constants';
 import { navigate_e } from '../models/core';
 
-let auth_xhr: Promise<unknown> | null = null;
+let auth_xhr: Promise<{ data: { token: string } }> | null = null;
 let auth_done: boolean = false;
+let token: string | null = null;
 
 
 const req = <T>(url: string, init?: RequestInit): Promise<ParsedResponse<T>> =>
     fetch(`${window.location.origin}${url}`, {
         method: init?.method ?? 'GET',
         headers: Object.assign({
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-Session-Token': token ?? void 0
         }, init?.headers ?? {}),
         ...(init ?? {})
     }).then<ParsedResponse<T>>(parse_response);
@@ -37,17 +39,18 @@ export const parse_response = <T>(r: Response): Promise<ParsedResponse<T>> =>
             .then(pipe(add_content(r), reject));
 
 const auth = () => {
-    auth_xhr = req('/api/v1/web-app/user/auth', {
+    auth_xhr = req<{ token: string }>('/api/v1/web-app/user/auth', {
         method: 'POST',
         body: JSON.stringify({ auth_data: Telegram.WebApp.initData })
     }).catch(() => {
         navigate_e(`${BASE_URL}/403`);
-        return void 0;
+        return { data: { token: '' } };
     });
 
-    auth_xhr.then(() => {
+    auth_xhr.then((response) => {
         auth_xhr = null;
         auth_done = true;
+        token = response.data.token;
     });
 
     return auth_xhr;
