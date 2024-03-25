46052d99f01c08b875e9689bf11c0899cb1ffb43
tsigel <tsddaniil@gmail.com>
2024-03-25 19:22:21 +0300

    Fix queueMicrotask

--- client/src/utils/globalThis.ts
+++ client/src/utils/globalThis.ts
@@ -1,3 +1,11 @@
 if (typeof globalThis === 'undefined') {
     (window as any).globalThis = window;
 }
+
+if (typeof window.queueMicrotask !== "function") {
+    window.queueMicrotask = function (callback) {
+        Promise.resolve()
+            .then(callback)
+            .catch(e => setTimeout(() => { throw e; }));
+    };
+}
