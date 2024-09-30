73963ddb5aae0d6ceab4c9fb2e500847fe313e4c
tsigel <tsddaniil@gmail.com>
2024-09-30 20:35:54 +0300

    Fix time table

--- backend/src/telegram/handlers/get_time_table.ts
+++ backend/src/telegram/handlers/get_time_table.ts
@@ -3,15 +3,21 @@ import { get_student_by_tg } from '../../utils/get_student_by_tg';
 import { get_manager } from '../../utils/request_mk';
 import { MessageSpliter } from '../../services/MessageSpliter';
 import { getFixedT, t } from 'i18next';
-import { always, indexBy, isNotNil, pipe, prop, propEq } from 'ramda';
+import { always, filter, indexBy, isNotNil, not, pipe, prop, propEq } from 'ramda';
 import { error } from '../../utils/log';
 import { TG_MK_ADMIN_USER } from '../../constants';
 import { get_lessons } from './utils/get_lessons';
 
+const get_student = (user: User | number) =>
+    typeof user === 'number'
+        ? Promise.resolve(user)
+        : get_student_by_tg(user.id, true);
+
 export const get_time_table =
-    (user: User) =>
-        get_student_by_tg(user.id, true)
+    (user: User | number) =>
+        get_student(user)
             .then(get_lessons)
+            .then(filter(pipe(propEq('b4b', 'filial'), not)))
             .then((lessons) => {
                 const t = getFixedT('ru', undefined, 'telegram.actions.timetable');
 
